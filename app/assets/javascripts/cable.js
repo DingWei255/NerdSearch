44f7b99c7163101b8196951201fa9090033b39ce
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2020-07-25 07:17:57 +0000

    Fix test cases.

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,2 +1,5 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features live usi
+// You can generate new channels where WebSocket features live using the `rails generate channel` command.
+//
+//= require action_cable
+//= re
