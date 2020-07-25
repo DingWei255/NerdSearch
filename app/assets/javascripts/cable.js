49d443ae9e9151c7dab6e15398b15ab9d2d404d0
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2020-07-25 14:43:26 +0000

    go mod

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -2,4 +2,8 @@
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 //
 //= require action_cable
-//= re
+//= require_self
+//= require_tree ./channels
+
+(function() {
+  t
