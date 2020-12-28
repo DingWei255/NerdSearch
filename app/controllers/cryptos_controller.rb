8dd272f321160d964adffdc95ff653e855009f4c
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2020-12-28 17:18:46 +0000

    Changed footer text color and font size

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,4 +1,8 @@
 class CryptosController < ApplicationController
   before_action :set_crypto, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_action :correct_user, only: [:show, :edit, :update, :destroy
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  # GET /cryptos
+  # GET /cryptos.json
+  def index
+    @cryptos = C
