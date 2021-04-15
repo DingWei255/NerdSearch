1550050c9c8be63085c41c57807a5648bd73a1ee
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2021-04-15 21:25:55 +0000

    removed unused image

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -79,4 +79,7 @@ class CryptosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_crypto
- 
+      @crypto = Crypto.find(params[:id])
+    end
+
+    # Never
