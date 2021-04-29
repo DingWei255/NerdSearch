e8269075e5f1075d50c62f0f7388c372d6d95126
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2021-04-29 06:20:31 +0000

    staging: android: ashmem: add missing include

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,6 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require'json'
-    @url = 'https://api.coinmarketcap.com/v1/tic
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri
