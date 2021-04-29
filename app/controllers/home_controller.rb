d675035332915c4e9c97a8c09ec35dd3a1ea476d
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2021-04-29 06:55:19 +0000

    Fixed format

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,6 @@ class HomeController < ApplicationController
     require'json'
     @url = 'https://api.coinmarketcap.com/v1/ticker/'
     @uri = URI(@url)
-    @response = Net::HTTP.get(@uri
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["B
