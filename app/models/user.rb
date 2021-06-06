7e3b689cc2349f3980447b8e0055e5ccd12b57c1
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2021-06-06 17:16:24 +0000

    net: Swap ver and type in pppoe_hdr

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    
+         :recoverable, :rememberable, :validatable
+end
