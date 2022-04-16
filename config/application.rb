ff22e3c48d9fc99291be81d8d77a62d2dc006bb8
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-04-16 11:32:06 +0000

    bump x.y.z versions to even y prior to creation of 3_5 branch

--- config/application.rb
+++ config/application.rb
@@ -3,4 +3,6 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/rai
+require "active_job/railtie"
+require "active_record/railtie"
+require "acti
