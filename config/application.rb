727800da5a4b2b2d4c1e6221f25c8aee918a1b00
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-04-20 17:15:04 +0000

    Updated dependency

--- config/application.rb
+++ config/application.rb
@@ -7,4 +7,6 @@ require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
-require "action_mail
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "actio
