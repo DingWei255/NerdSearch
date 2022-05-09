6e54a4aa8e6b40dbb6b425336593a140376abf15
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-05-09 10:27:11 +0000

    Deleting unnecessary files

--- config/application.rb
+++ config/application.rb
@@ -27,4 +27,5 @@ module CryptoPort
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-   
+    # Don't generate system test files.
+    config.generators.system_tests =
