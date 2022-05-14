73892721647735191c0524fec90408be908e3fe3
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-05-14 08:30:20 +0000

    Update readme title

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Ge
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by c
