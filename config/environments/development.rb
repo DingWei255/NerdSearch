a660fa29b796f16fb72a6f4d770b94cc189db361
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-05-28 15:12:17 +0000

    Added config file.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,4 +18,5 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
-    config.public
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age
