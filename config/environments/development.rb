762a7d9fd9539f65a479c77a41b58ed74947bf24
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-05-29 19:41:11 +0000

    update resources

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,5 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see conf
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage
