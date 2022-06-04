bc521e1e36d64fd17473fd1ae89107738d2dc26e
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-06-04 13:20:01 +0000

    Version bump to 0.5.6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Print d
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
