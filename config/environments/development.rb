bc4717bf560824ddab258413031588a0ff708768
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-06-08 08:17:03 +0000

    chore(): update changelog

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,3 +47,6 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  config.assets.debug = true
+
+  # Suppress logger output for asset
