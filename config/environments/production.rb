a02aa236740cdf17d1f7fb69e252fedc2d244295
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-06-13 14:34:42 +0000

    updated docu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not r
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  # Eager 
