0774be42cc38920a665148581be934f6b05492a2
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-06-26 05:49:14 +0000

    f2fs: split sync_node_pages with fsync_node_pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,4 +22,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # 
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+ 
