8d48568441c5ac0b6717790761bacc94adde4292
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-07-24 13:32:43 +0000

    Update launcher icon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_
+  # config.active_job.queue_name_prefix = "CryptoPort_#{Rails.env}"
+
+  config.action_mailer.p
