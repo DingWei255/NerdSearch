21a2dabd5fd040b3029e86834f6bc209dbd7b728
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-08-01 19:30:35 +0000

    new config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatte
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatte
