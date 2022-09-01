7bd2dda1ea4122550ddca9e237e95e2296274a30
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-09-01 21:01:29 +0000

    Removing duplicate code

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,4 +6,6 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
-#   inflect
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# 
