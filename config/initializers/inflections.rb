b7f6fd2e3bf1a690276f9098fd5f180e523dc02b
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-09-02 12:37:45 +0000

    Linux 3.10.93

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -12,4 +12,5 @@
 
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acro
+#   inflect.acronym 'RESTful'
+# end
