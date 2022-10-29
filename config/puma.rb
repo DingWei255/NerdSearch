ecc1b5dd6e642dc410a2b35c39cb4b2a3551964f
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-10-29 06:17:54 +0000

    Fix build instructions

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thr
+# Any libraries that use thread pools should be configured to match
+# the maximum value spe
