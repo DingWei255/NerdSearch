b60426dfc2f2e57fc721296d13e43d2588a8bdbf
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-11-02 12:39:48 +0000

    fix layout issue

--- config/puma.rb
+++ config/puma.rb
@@ -11,4 +11,6 @@ threads threads_count, threads_count
 #
 port        ENV.fetch("PORT") { 3000 }
 
-# Specifies the `environment` th
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "devel
