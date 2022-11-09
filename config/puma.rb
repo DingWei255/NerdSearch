8f90364b4cf2bab1d8bbd68f4b61167e53d8ffd2
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-11-09 07:38:33 +0000

    Add missing property

--- config/puma.rb
+++ config/puma.rb
@@ -24,4 +24,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot
+# This directive tells Puma to first boot the application and load code
+# before forking the application
