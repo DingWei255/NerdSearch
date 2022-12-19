b891adea63e7f349b1c365465276e13fbae0ee73
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-12-19 16:57:45 +0000

    Update guloader.txt

--- db/migrate/20181107184622_devise_create_users.rb
+++ db/migrate/20181107184622_devise_create_users.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
-  def
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+     
