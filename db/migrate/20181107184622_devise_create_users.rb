ebd4f6b0e910f31fafc88070971673075f1c08ac
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-12-24 11:55:54 +0000

    lilypond-0.0.21

--- db/migrate/20181107184622_devise_create_users.rb
+++ db/migrate/20181107184622_devise_create_users.rb
@@ -16,4 +16,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.inet     :c
