bed4d41c72c2ec3cfed68e54dd83a761d9ef89ad
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-12-25 07:47:11 +0000

    bad import

--- db/migrate/20181107184622_devise_create_users.rb
+++ db/migrate/20181107184622_devise_create_users.rb
@@ -25,4 +25,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
-      #
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## L
