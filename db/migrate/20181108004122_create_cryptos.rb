0d34d0d343aaf94d547528d53e615361716a94eb
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2022-12-29 19:14:57 +0000

    Removed old file

---
+++ db/migrate/20181108004122_create_cryptos.rb
@@ -0,0 +1,12 @@
+
+class CreateCryptos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cryptos do |t|
+      t.string :symbol
+      t.integer :user_id
+      t.decimal :cost_per
+      t.decimal :amount_owned
+
+      t.timestamps
+    end
+    add_index :cryptos, :user_id
