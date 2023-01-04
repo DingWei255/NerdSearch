e11cd43b2fd21caf1eaa318711667ddd14f9548e
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2023-01-04 06:19:57 +0000

    working on zwatch

--- db/schema.rb
+++ db/schema.rb
@@ -1,2 +1,3 @@
 # This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrati
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your d
