0fc15ccae502c83ec38154e6f91dc09797dc2223
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2023-01-12 22:32:12 +0000

    Remove debug messages.

--- db/schema.rb
+++ db/schema.rb
@@ -8,4 +8,4 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's stro
+# It's strongly recommended that you check this file into your version cont
