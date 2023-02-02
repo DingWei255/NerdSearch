d57de5b35b03464349d34b047272263c829e4890
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2023-02-02 22:01:46 +0000

    2.7.7

--- db/schema.rb
+++ db/schema.rb
@@ -33,4 +33,5 @@ ActiveRecord::Schema.define(version: 2018_11_08_004122) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], nam
