48dd361375999a8b02e7ea3bc9f3a46414260e69
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2023-02-11 08:19:19 +0000

    npm deploy

--- db/schema.rb
+++ db/schema.rb
@@ -34,4 +34,4 @@ ActiveRecord::Schema.define(version: 2018_11_08_004122) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], nam
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: 
