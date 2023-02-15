c4d395d11ddb25341ef85477521509b829db5b86
DingWei255 <106365731+DingWei255@users.noreply.github.com>
2023-02-15 13:51:12 +0000

    ext4: don't copy non-existent gdt blocks when resizing

--- db/schema.rb
+++ db/schema.rb
@@ -34,4 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_004122) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: 
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,1 @@
+# This file should contain all the record creation ne
